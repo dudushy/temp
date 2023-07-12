f28c5b91849a2f8609c65136dd4583a352a15eee
dudushy <eduardotalarico1337@gmail.com>
2023-07-12 10:17:12 -0300

    a lot of progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,12 @@ import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
 
 import { APP_VERSION } from 'src/environments/version';
 
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+
+import { File } from '@awesome-cordova-plugins/file/ngx';
+
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -27,8 +33,6 @@ export class AppComponent {
   APP_VERSION = APP_VERSION;
   HOME_PAGE = 'home';
   HOME_DIR = 'template';
-
-  window = window;
 
   devMode = false;
   mobileMode = false;
@@ -49,6 +53,9 @@ export class AppComponent {
     public events: EventsService,
     public loadingController: LoadingController,
     public backgroundMode: BackgroundMode,
+    public http: HTTP,
+    public file: File,
+    public camera: Camera,
   ) {
     console.log(`[${this.title}#constructor]`);
 
@@ -83,12 +90,18 @@ export class AppComponent {
       this.mobileMode = !this.platform.is('tablet');
       console.log(`[${this.title}#constructor/ready] mobileMode:`, this.mobileMode);
 
-      this.backgroundMode.enable();
-      this.backgroundMode.setEnabled(true);
-      console.log(`[${this.title}#constructor/ready] backgroundMode:`, this.backgroundMode.isEnabled(), this.backgroundMode.isActive());
-      this.backgroundMode.isScreenOff((bool) => {
-        console.log(`[${this.title}#constructor/ready] backgroundMode/isScreenOff:`, bool);
-      });
+      // try {
+      //   document.addEventListener('deviceready', () => {
+      //     this.backgroundMode.enable();
+      //     this.backgroundMode.setEnabled(true);
+      //     console.log(`[${this.title}#constructor/ready] backgroundMode:`, this.backgroundMode.isEnabled(), this.backgroundMode.isActive());
+      //     this.backgroundMode.isScreenOff((bool) => {
+      //       console.log(`[${this.title}#constructor/ready] backgroundMode/isScreenOff:`, bool);
+      //     });
+      //   }, false);
+      // } catch (error) {
+      //   console.log(`[${this.title}#constructor/ready] backgroundMode ERROR:`, error);
+      // }
 
       await this.alertDbCap();
     });
@@ -280,6 +293,111 @@ export class AppComponent {
 
       if (data[0]['COUNT(*)'] >= cap) {
         await this.showAlert('Quantidade de registros', `Você atingiu ${cap} registros. Por favor, limpe os dados para continuar utilizando o aplicativo com máxima perfomance.\nAcesse: MENU -> RELATORIO -> APAGAR SINCRONIZADOS`);
+      }
+    });
+  }
+
+  async takePhoto(varname: any, mode = 'base64'): Promise<any> {
+    console.log(`[${this.title}#takePhoto] varname`, varname);
+    console.log(`[${this.title}#takePhoto] mode`, mode);
+
+    let result: any = null;
+
+    try {
+      const options: CameraOptions = {
+        quality: 100,
+        // quality: 60,
+        // targetWidth: 500,
+        // targetHeight: 500,
+        destinationType: mode == 'base64' ? this.camera.DestinationType.DATA_URL : this.camera.DestinationType.FILE_URI, //! BASE64
+        // destinationType: this.camera.DestinationType.DATA_URL, //! BASE64
+        // destinationType: this.camera.DestinationType.FILE_URI, //! BLOB
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        correctOrientation: true,
+        sourceType: this.camera.PictureSourceType.CAMERA
+      };
+      console.log(`[${this.title}#takePhoto] options`, options);
+      // console.log(`[${this.title}#takePhoto] JSON(options)`, JSON.stringify(options));
+
+      await this.camera.getPicture(options).then(async (imageData) => {
+        console.log(`[${this.title}#takePhoto] imageData`, imageData);
+
+        if (mode == 'base64') {
+          const base64Image = 'data:image/jpeg;base64,' + imageData;
+          console.log(`[${this.title}#takePhoto] base64Image`, base64Image);
+
+          result = base64Image;
+        }
+
+        if (mode == 'blob') {
+          const path = imageData.split('/').slice(0, -1).join('/').concat('/');
+          console.log(`[${this.title}#takePhoto] path`, path);
+
+          const file = imageData.split('/').slice(-1)[0];
+          console.log(`[${this.title}#takePhoto] file`, file);
+
+          const arrayBuffer = await this.file.readAsArrayBuffer(path, file).then(arrayBuffer => { return arrayBuffer; });
+          console.log(`[${this.title}#takePhoto] arrayBuffer`, arrayBuffer);
+
+          const blob = new Blob([arrayBuffer], { type: 'image/png' });
+          console.log(`[${this.title}#takePhoto] blob`, blob);
+
+          result = blob;
+        }
+      }, (error) => {
+        console.log(`[${this.title}#takePhoto] error`, error);
+      });
+    } catch (error) {
+      console.log(`[${this.title}#takePhoto] error`, error);
+      await this.showAlert('Erro', `Não foi possível tirar a foto [${varname}], tente novamente!`);
+
+      result = null;
+    }
+
+    console.log(`[${this.title}#takePhoto] result`, result);
+    return new Promise((resolve) => resolve(result));
+  }
+
+  async blobToFile(blob: Blob, filename: string, foldername: string, recursive = false): Promise<any> {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async (resolve) => {
+      console.log(`[${this.title}#blobToFile] --------------------------------------------------`);
+      console.log(`[${this.title}#blobToFile] blob`, blob);
+      console.log(`[${this.title}#blobToFile] filename`, filename);
+      console.log(`[${this.title}#blobToFile] foldername`, foldername);
+      // console.log(`[${this.title}#blobToFile] recursive`, recursive);
+
+      try {
+        await this.file.checkDir(this.file.dataDirectory, this.HOME_DIR).then(async () => {
+          console.log(`[${this.title}#blobToFile] checkDir OK: `, [this.file.dataDirectory, this.HOME_DIR]);
+
+          await this.file.createDir(`${this.file.dataDirectory}${this.HOME_DIR}`, foldername, true).then(async () => {
+            console.log(`[${this.title}#blobToFile] createDir [${foldername}] OK: `, [`${this.file.dataDirectory}${this.HOME_DIR}`, foldername, true]);
+
+            await this.file.writeFile(`${this.file.dataDirectory}${this.HOME_DIR}/${foldername}`, filename, blob, { replace: true }).then(async () => {
+              console.log(`[${this.title}#blobToFile] writeFile OK: `, [`${this.file.dataDirectory}${this.HOME_DIR}/${foldername}`, filename, blob, { replace: true }]);
+
+              await this.file.checkFile(`${this.file.dataDirectory}${this.HOME_DIR}/${foldername}/`, filename).then(() => {
+                console.log(`[${this.title}#blobToFile] checkFile [${filename}] OK: `, [`${this.file.dataDirectory}${this.HOME_DIR}/${foldername}/`, filename]);
+
+                const finalPath = this.file.dataDirectory + `${this.HOME_DIR}/${foldername}/${filename}`;
+                console.log(`[${this.title}#blobToFile] finalPath`, finalPath);
+
+                resolve(finalPath);
+              });
+            });
+          });
+        });
+      } catch (error) {
+        console.log(`[${this.title}#blobToFile] ERROR:`, error);
+
+        await this.file.createDir(this.file.dataDirectory, this.HOME_DIR, true).then(async () => {
+          console.log(`[${this.title}#blobToFile] createDir [${this.HOME_DIR}] OK`);
+
+          console.log(`[${this.title}#blobToFile] recursive`, recursive);
+          if (!recursive) resolve(await this.blobToFile(blob, filename, foldername, true));
+        });
       }
     });
   }
--- src/app/pages/test/test.page.ts
+++ src/app/pages/test/test.page.ts
@@ -5,12 +5,6 @@ import { AppComponent } from 'src/app/app.component';
 
 import { Platform } from '@ionic/angular';
 
-import { HTTP } from '@awesome-cordova-plugins/http/ngx';
-
-import { File } from '@awesome-cordova-plugins/file/ngx';
-
-import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
-
 @Component({
   selector: 'app-test',
   templateUrl: './test.page.html',
@@ -19,13 +13,13 @@ import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 export class TestPage implements OnInit {
   title = 'test';
 
+  testCameraResult: any = null;
+  testRequestResult: any = null;
+
   constructor(
     public app: AppComponent,
     public platform: Platform,
     private cdr: ChangeDetectorRef,
-    public http: HTTP,
-    public file: File,
-    public camera: Camera,
   ) {
     console.log(`[${this.title}#constructor]`);
   }
@@ -56,109 +50,56 @@ export class TestPage implements OnInit {
     this.app.redirectTo(this.title, url);
   }
 
-  async takePhoto(varname: any, mode = 'base64') {
-    console.log(`[${this.title}#takePhoto] varname`, varname);
+  async testCamera() {
+    console.log(`[${this.title}#testCamera] (BEFORE) testCameraResult:`, this.testCameraResult);
 
-    let result: any = null;
+    // const result = await this.app.takePhoto('test');
+    // console.log(`[${this.title}#testCamera] result:`, result);
+    // this.testCameraResult = result;
 
-    try {
-      const options: CameraOptions = {
-        quality: 100,
-        // quality: 60,
-        // targetWidth: 500,
-        // targetHeight: 500,
-        destinationType: mode == 'base64' ? this.camera.DestinationType.DATA_URL : this.camera.DestinationType.FILE_URI, //! BASE64
-        // destinationType: this.camera.DestinationType.DATA_URL, //! BASE64
-        // destinationType: this.camera.DestinationType.FILE_URI, //! BLOB
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE,
-        correctOrientation: true,
-        sourceType: this.camera.PictureSourceType.CAMERA
-      };
-      console.log(`[${this.title}#takePhoto] options`, options);
-      console.log(`[${this.title}#takePhoto] JSON(options)`, JSON.stringify(options));
+    const result = await this.app.takePhoto('test', 'blob');
+    console.log(`[${this.title}#testCamera] result:`, result);
 
-      await this.camera.getPicture(options).then(async (imageData) => {
-        console.log(`[${this.title}#takePhoto] (BEFORE) result:`, result);
-        console.log(`[${this.title}#takePhoto] imageData`, imageData);
-        console.log(`[${this.title}#takePhoto] mode`, mode);
+    const finalPath = await this.app.blobToFile(result, 'test_camera.png', 'test');
+    console.log(`[${this.title}#takePhoto] finalPath`, finalPath);
 
-        if (mode == 'base64') {
-          const base64Image = 'data:image/jpeg;base64,' + imageData;
-          console.log(`[${this.title}#takePhoto] base64Image`, base64Image);
+    const win: any = window;
+    const safeURL = win.Ionic.WebView.convertFileSrc(finalPath);
+    console.log(`[${this.title}#takePhoto] safeURL`, safeURL);
 
-          result = base64Image;
-        }
-
-        if (mode == 'blob') {
-          const path = imageData.split('/').slice(0, -1).join('/').concat('/');
-          console.log(`[${this.title}#takePhoto] path`, path);
-
-          const file = imageData.split('/').slice(-1)[0];
-          console.log(`[${this.title}#takePhoto] file`, file);
-
-          const arrayBuffer = await this.file.readAsArrayBuffer(path, file).then(arrayBuffer => { return arrayBuffer; });
-          console.log(`[${this.title}#takePhoto] arrayBuffer`, arrayBuffer);
-
-          const blob = new Blob([arrayBuffer], { type: 'image/png' });
-          console.log(`[${this.title}#takePhoto] blob`, blob);
-
-          result = blob;
-        }
-      }, (error) => {
-        console.log(`[${this.title}#takePhoto] error`, error);
-      });
-    } catch (error) {
-      console.log(`[${this.title}#takePhoto] error`, error);
-      await this.app.showAlert('Erro', `Não foi possível tirar a foto [${varname}], tente novamente!`);
-
-      result = null;
-    }
-
-    console.log(`[${this.title}#takePhoto] result`, result);
-    return result;
+    this.testCameraResult = safeURL + `?temp=${new Date().getTime()}`;
+    console.log(`[${this.title}#testCamera] (AFTER) testCameraResult:`, this.testCameraResult);
   }
 
-  async blobToFile(blob: Blob, filename: string, foldername: string, recursive = false): Promise<any> {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve) => {
-      console.log(`[${this.title}#blobToFile] --------------------------------------------------`);
-      console.log(`[${this.title}#blobToFile] blob`, blob);
-      console.log(`[${this.title}#blobToFile] filename`, filename);
-      console.log(`[${this.title}#blobToFile] foldername`, foldername);
-      // console.log(`[${this.title}#blobToFile] recursive`, recursive);
+  async testRequest() {
+    console.log(`[${this.title}#testRequest] GET`);
 
-      try {
-        await this.file.checkDir(this.file.dataDirectory, this.app.HOME_DIR).then(async () => {
-          console.log(`[${this.title}#blobToFile] checkDir OK: `, [this.file.dataDirectory, this.app.HOME_DIR]);
+    await this.app.startLoading('Baixando...');
 
-          await this.file.createDir(`${this.file.dataDirectory}${this.app.HOME_DIR}`, foldername, true).then(async () => {
-            console.log(`[${this.title}#blobToFile] createDir [${foldername}] OK: `, [`${this.file.dataDirectory}${this.app.HOME_DIR}`, foldername, true]);
+    const url = 'https://random-word-api.vercel.app/api?words=10';
+    const headers = {};
+    const body = {};
 
-            await this.file.writeFile(`${this.file.dataDirectory}${this.app.HOME_DIR}/${foldername}`, filename, blob, { replace: true }).then(async () => {
-              console.log(`[${this.title}#blobToFile] writeFile OK: `, [`${this.file.dataDirectory}${this.app.HOME_DIR}/${foldername}`, filename, blob, { replace: true }]);
+    console.log(`[${this.title}#testRequest] url`, url);
+    console.log(`[${this.title}#testRequest] headers`, headers);
+    console.log(`[${this.title}#testRequest] body`, body);
 
-              await this.file.checkFile(`${this.file.dataDirectory}${this.app.HOME_DIR}/${foldername}/`, filename).then(() => {
-                console.log(`[${this.title}#blobToFile] checkFile [${filename}] OK: `, [`${this.file.dataDirectory}${this.app.HOME_DIR}/${foldername}/`, filename]);
+    this.app.http.setDataSerializer('json');
+    await this.app.http.get(url, body, headers)
+      .then(async response => {
+        console.log(`[${this.title}#testRequest] response`, response);
+        console.log(`[${this.title}#testRequest] response.data`, JSON.parse(response.data));
 
-                const finalPath = this.file.dataDirectory + `${this.app.HOME_DIR}/${foldername}/${filename}`;
-                console.log(`[${this.title}#blobToFile] finalPath`, finalPath);
+        this.testRequestResult = JSON.parse(response.data);
 
-                resolve(finalPath);
-              });
-            });
-          });
-        });
-      } catch (error) {
-        console.log(`[${this.title}#blobToFile] ERROR:`, error);
+        await this.app.stopLoading();
+        this.updateView();
+      })
+      .catch(async error => {
+        console.log(`[${this.title}#testRequest] error`, error);
 
-        await this.file.createDir(this.file.dataDirectory, this.app.HOME_DIR, true).then(async () => {
-          console.log(`[${this.title}#blobToFile] createDir [${this.app.HOME_DIR}] OK`);
-
-          console.log(`[${this.title}#blobToFile] recursive`, recursive);
-          if (!recursive) resolve(await this.blobToFile(blob, filename, foldername, true));
-        });
-      }
-    });
+        await this.app.stopLoading();
+        this.updateView();
+      });
   }
 }
